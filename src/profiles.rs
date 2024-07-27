9b0b1a609688106cf4661897b8c9066397647ea8
Evgeny Fomin <fominok@hotmail.com>
2024-07-27 05:33:30 +0200

    fix drive profile for root tree

--- src/profiles.rs
+++ src/profiles.rs
@@ -38,23 +38,25 @@ impl<'c> EnabledProfile<'c> {
 pub(crate) fn drive_profile() -> Profile<impl Iterator<Item = (Vec<Vec<u8>>, String)> + Clone> {
     Profile::new(
         [
-            (vec![vec![32]], "Identities".to_string()),
             (vec![vec![64]], "Data contract documents".to_string()),
+            (vec![vec![32]], "Identities".to_string()),
             (
                 vec![vec![24]],
                 "Unique public key hashes to identities".to_string(),
             ),
-            (vec![vec![72]], "Spent asset lock transactions".to_string()),
-            (vec![vec![48]], "Pools".to_string()),
-            (vec![vec![112]], "Misc".to_string()),
-            (vec![vec![80]], "Withdrawal transactions".to_string()),
-            (vec![vec![96]], "Balances".to_string()),
-            (vec![vec![16]], "Token balances".to_string()),
             (
                 vec![vec![8]],
                 "Non-unique public key Key hashes to identities".to_string(),
             ),
+            (vec![vec![48]], "Pools".to_string()),
+            (vec![vec![40]], "Pre funded specialized balances".to_string()),
+            (vec![vec![72]], "Spent asset lock transactions".to_string()),
+            (vec![vec![104]], "Misc".to_string()),
+            (vec![vec![80]], "Withdrawal transactions".to_string()),
+            (vec![vec![96]], "Balances".to_string()),
+            (vec![vec![16]], "Token balances".to_string()),
             (vec![vec![120]], "Versions".to_string()),
+            (vec![vec![112]], "Votes".to_string()),
         ]
         .into_iter(),
     )
